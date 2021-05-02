7c99734095b4084fa1a4ac1a0ec43b41a1d14f01
이상준 <dltkd1214@naver.com>
2021-05-02 23:31:58 +0900

    feat:게임룰 추가

---
+++ src/main/java/com/tistory/ddoriya/constants/GameRule.java
@@ -0,0 +1,14 @@
+/*
+ * @(#) GameRule.java 2021. 04. 23.
+ */
+package com.tistory.ddoriya.constants;
+
+/**
+ * @author 이상준
+ */
+public class GameRule {
+	static final public int RACING_START = 0;
+	static final public int RACING_MAX_NUMBER = 9;
+	static final public int RACING_CAR_NAME_MAX_LENGTH = 5;
+	static final public String RACING_CAR_MOVE_TEXT = "-";
+}
